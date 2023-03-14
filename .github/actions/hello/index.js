b809570717ad102e5781f0b4587d208547a00def
しも <56472405+Shimo74@users.noreply.github.com>
2023-03-15 03:07:29 +0900

    Update index.js

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,3 +1,5 @@
-// .github/actions/hello/index.js
-console.log("SECRET_HOGE_1 = ", process.env.SECRET_HOGE_1); 
-console.log("SECRET_HOGE_2 = ", process.env.SECRET_HOGE_2);
+// // .github/actions/hello/index.js
+// console.log("SECRET_HOGE_1 = ", process.env.SECRET_HOGE_1); 
+// console.log("SECRET_HOGE_2 = ", process.env.SECRET_HOGE_2);
+const secret = process.argv[2];
+console.log(`The value of TEST_SECRET is: ${secret}`);
