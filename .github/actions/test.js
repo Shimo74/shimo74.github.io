4b5f5edf079e69f210a839876428eeaaf20ca79d
しも <56472405+Shimo74@users.noreply.github.com>
2023-03-15 03:56:15 +0900

    Update test.js

--- .github/actions/test.js
+++ .github/actions/test.js
@@ -1,5 +1,2 @@
-// // .github/actions/hello/index.js
-// console.log("SECRET_HOGE_1 = ", process.env.SECRET_HOGE_1); 
-// console.log("SECRET_HOGE_2 = ", process.env.SECRET_HOGE_2);
-const secret = process.argv[2];
-console.log(`The value of TEST_SECRET is: ${secret}`);
+const secret = process.env.TEST_SECRET;
+document.getElementById('secret').textContent = secret;
